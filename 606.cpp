08ab66221b1f5fd69b84266504529f48cf6c8941
Dat-se40 <tandatthtt@gmail.com>
2024-12-03 08:39:33 +0700

    606.cpp
    Tìm giá trị lớn nhất (max) của mảng, cho biết trong mảng có bao nhiêu phần tử có
    
    giá trị bằng max.
    
    Input
    
    -       Nhập số phần tử của mảng.
    
    -       Nhập mảng số nguyên.
    
    Output
    
    -       Giá trị max
    
    -       Số phần tử bằng max.

---
+++ 606.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std ;
+int main (){
+  int n ;
+  cin>>n;
+  int arr[n]= {}, cnt[100] ={0} ;
+  for ( int i =0 ; i < n ; i++) {
+    cin>>arr[i];
+    cnt[arr[i]] = 0;
+  }
+  int maxx = arr[0];
+  for ( int i = 0 ; i < n ; i++ ){
+    maxx = (maxx >=  arr[i]) ? maxx : arr[i];
+    if( maxx == arr[i] )  cnt[maxx]++;
+  }
+  cout<<maxx<<endl<<cnt[maxx];
+  return 0 ;
+}
